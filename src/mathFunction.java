137f5b6d920c0937eeeea45c46974c8ee3927742
ksoorya <iamksoorya@gmail.com>
2021-04-09 16:19:37 +0530

    the file name is changed and lcm method is added

---
+++ src/mathFunction.java
@@ -0,0 +1,82 @@
+public class mathFunction {
+    void prime(int number) {
+        int flag = 0;
+        int half = number / 2;
+        int i;
+        if (number == 0 || number == 1 || number < 0) {
+            System.out.println(number + " is not a prime number ");
+        } else {
+            for (i = 2; i < half; i++) {
+                if (number % i == 0) {
+                    flag = flag + 1;
+                    break;
+                }
+            }
+        }
+        if (flag == 0) {
+            System.out.println(number + " is the prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+
+    long lcm_of_array_elements(int[] element_array) {
+        long lcm_of_array_elements = 1;
+        int divisor = 2;
+
+        while (true) {
+            int counter = 0;
+            boolean divisible = false;
+
+            for (int i = 0; i < element_array.length; i++) {
+
+                // lcm_of_array_elements (n1, n2, ... 0) = 0.
+                // For negative number we convert into
+                // positive and calculate lcm_of_array_elements.
+
+                if (element_array[i] == 0) {
+                    return 0;
+                } else if (element_array[i] < 0) {
+                    element_array[i] = element_array[i] * (-1);
+                }
+                if (element_array[i] == 1) {
+                    counter++;
+                }
+
+                // Divide element_array by devisor if complete
+                // division i.e. without remainder then replace
+                // number with quotient; used for find next factor
+                if (element_array[i] % divisor == 0) {
+                    divisible = true;
+                    element_array[i] = element_array[i] / divisor;
+                }
+            }
+
+            // If divisor able to completely divide any number
+            // from array multiply with lcm_of_array_elements
+            // and store into lcm_of_array_elements and continue
+            // to same divisor for next factor finding.
+            // else increment divisor
+            if (divisible) {
+                lcm_of_array_elements = lcm_of_array_elements * divisor;
+            } else {
+                divisor++;
+            }
+
+            // Check if all element_array is 1 indicate
+            // we found all factors and terminate while loop.
+            if (counter == element_array.length) {
+                return lcm_of_array_elements;
+            }
+        }
+    }
+        public static void main(String[] args){
+            int[] array = new int[]{22,24,12};
+            mathFunction object = new mathFunction();
+            System.out.println(object.lcm_of_array_elements(array));
+
+        }
+
+
+}
+
